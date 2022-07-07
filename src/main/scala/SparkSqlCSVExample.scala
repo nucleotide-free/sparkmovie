3cb1152a71a2d91cf5c2a48ddefeb81316c9d185
nucleotide <13713754853@163.com>
2022-07-07 12:41:17 +0800

    需求1 semi done

--- src/main/scala/SparkSqlCSVExample.scala
+++ src/main/scala/SparkSqlCSVExample.scala
@@ -1,5 +1,10 @@
+import net.minidev.json.{JSONArray, JSONObject}
+import net.minidev.json.parser.JSONParser
+
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.{DataFrame, Dataset, Row, SaveMode, SparkSession}
+import com.alibaba.fastjson.JSON
+import scala.xml.XML.parser
 
 /**
  * 知识点:
@@ -20,6 +25,7 @@ import org.apache.spark.sql.{DataFrame, Dataset, Row, SaveMode, SparkSession}
 object SparkSqlCSVExample {
 
 
+
   def main(args: Array[String]): Unit = {
     //1.创建Spark环境配置对象
     val conf = new SparkConf().setAppName("SparkSqlMovie").setMaster("local")
@@ -30,26 +36,43 @@ object SparkSqlCSVExample {
 
     var commentData: DataFrame = spark.read.format("csv")
       .option("header", true)
-      .option("multiLine", true)
       .load("src\\input\\movies_metadata.csv")
     commentData.show()
+
     //3.注册临时表
     commentData.createOrReplaceTempView("tbl_movies")
     //4.查询操作
-    val sqlresult : DataFrame=
-      spark.sql("select * " +
+    val sqlresult_type :DataFrame=
+      spark.sql("select genres " +
         "   from tbl_movies ")
+    var array = sqlresult_type.collect
+    val parser = new JSONParser(JSONParser.BIG_DIGIT_UNRESTRICTED)
+    for(i <- 0 to array.length-1){
+      for(j <- 0 to array(i).length-1){
+        val jsonArray = array(i)(j).toString
+        val parseJsonArray = JSON.parseArray(jsonArray)
+        //遍历
+        for (i <- 0 until parseJsonArray.size){
+          val jsonObject = parseJsonArray.getJSONObject(i)
+          val id = jsonObject.getInteger("id")
+          val name = jsonObject.getString("name")
+          println(s"$id is $name")
+        }
+      }
+    }
 
-    sqlresult.show()
-    //5.将分析结果保存到数据表中
-    //       eg:存入oa数据库中
-    sqlresult.write
-      .format("jdbc")
-      .option("url","jdbc:mysql://localhost:3306/sparkdb")
-      .option("user","root")
-      .option("password","100708007sM" )
-      .option("dbtable","tbl_movies_process_result")
-      .mode(SaveMode.Append)
-      .save()
+
+
+//    sqlresult_type.show()
+//    //5.将分析结果保存到数据表中
+//    //       eg:存入oa数据库中
+//    sqlresult_type.write
+//      .format("jdbc")
+//      .option("url","jdbc:mysql://localhost:3306/sparkdb")
+//      .option("user","root")
+//      .option("password","100708007sM" )
+//      .option("dbtable","tbl_movies_process_result")
+//      .mode(SaveMode.Append)
+//      .save()
   }
 }
